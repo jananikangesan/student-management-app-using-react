1019eac609e1a3e4a10ab070139fb7805790a9b4
jananikangesan <janakanges1996@gmail.com>
2024-05-10 09:24:21 +0530

    student is created

--- src/components/CreateStudentComponent.js
+++ src/components/CreateStudentComponent.js
@@ -1,93 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import StudentService from '../services/StudentService';
+import { useNavigate } from 'react-router-dom';
 
-class CreateStudentComponent extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state={
-            name:'',
-            address:''
-        }
-        this.changeNameHandler=this.changeNameHandler.bind(this);
-        this.changeAddressHandler=this.changeAddressHandler.bind(this);
-        this.saveStudent=this.saveStudent.bind(this);
-    }
+const CreateStudentComponent = () => {
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const navigate = useNavigate();
 
-    saveStudent=(e)=>{
+    const saveStudent = (e) => {
         e.preventDefault();
+        const student = { name, address };
+        console.log("student => ", student);
 
-        let student={name:this.state.name, address:this.state.address};
-        console.log("student => " + JSON.stringify(student));
-
-        StudentService.createStudent(student).then(res=>{
-           
-            this.setState({
-                name: '',
-                address: ''
+        StudentService.createStudent(student)
+            .then(res => {
+                console.log("result:", res);
+                navigate("/");
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                
             });
-            console.log("res:" + res);
-        })
-        .catch(error => {
-            console.error("Error:", error);
-            // Handle error, if needed
-        });
-    
-    }
+    };
 
-    changeNameHandler=(event)=>{
-        this.setState({name:event.target.value});
-    }
+    const changeNameHandler = (event) => {
+        setName(event.target.value);
+    };
 
-    changeAddressHandler=(event)=>{
-        this.setState({address:event.target.value});
-    }
+    const changeAddressHandler = (event) => {
+        setAddress(event.target.value);
+    };
 
-    handleCancel = () => {
-        this.setState({ name:'',
-                        address:''
-                     });
+    const handleCancel = () => {
+        setName('');
+        setAddress('');
+        navigate("/add-student");
+    };
 
-      };
-
-    render() {
-        return (
-            <div>
-                <div className='container pt-5'>
-                    <div className='row'>
-                        <div className='card col-md-6 offset-md-3 offset-md-3'>
-                            <h3 className='text-center'>Add Student</h3>
-                            <div className='card-body'>
-                                <form>
-                                    <div className='form-group'>
-                                        <label>Name</label>
-                                        <input placeholder='name'  name="name" className='form-control' value={this.state.name} onChange={this.changeNameHandler}/>
-
-                                    </div>
-                                    <div className='form-group'>
-                                        <label>Address</label>
-                                        <input placeholder='address'  name="address" className='form-control' value={this.state.address} onChange={this.changeAddressHandler}/>
-
-                                    </div>
-                                    <div className='pt-2'>
-                                        <button className='btn btn-success' onClick={this.saveStudent}>Save</button>
-                                        <button className='btn btn-danger' onClick={this.handleCancel} style={{marginLeft:"10px"}}>Cancel</button>
-                                    </div>
-                                   
-
-                                </form>
-
-                            </div>
-
+    return (
+        <div>
+            <div className='container pt-5'>
+                <div className='row'>
+                    <div className='card col-md-6 offset-md-3 offset-md-3'>
+                        <h3 className='text-center'>Add Student</h3>
+                        <div className='card-body'>
+                            <form>
+                                <div className='form-group'>
+                                    <label>Name</label>
+                                    <input placeholder='name' name="name" className='form-control' value={name} onChange={changeNameHandler} />
+                                </div>
+                                <div className='form-group'>
+                                    <label>Address</label>
+                                    <input placeholder='address' name="address" className='form-control' value={address} onChange={changeAddressHandler} />
+                                </div>
+                                <div className='pt-2'>
+                                    <button className='btn btn-success' onClick={saveStudent}>Save</button>
+                                    <button className='btn btn-danger' onClick={handleCancel} style={{ marginLeft: "10px" }}>Cancel</button>
+                                </div>
+                            </form>
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default CreateStudentComponent;
